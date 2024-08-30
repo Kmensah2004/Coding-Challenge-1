52b01ca22a85c686240b916e4764b25e8138e448
Kosi Mensah <kmensah@usf.edu>
2024-08-30 16:34:33 -0400

    "Task 2 - Product Inventory Management".

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,12 @@ var isActive = true;
 console.log(employeeName,typeof(employeeName));
 console.log(employeeID,typeof(employeeID));
 console.log(isActive,typeof(isActive));
+
+//Task 2: Compound Data Types
+let products = ["brownies","cookies","cakes"];
+const productDetails = {
+name: "brownies",
+price: 540,
+inStock:true,
+}
+console.log(products,productDetails);
