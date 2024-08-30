92092892cba484a2e575af1aa580241cfea5bd50
Kosi Mensah <kmensah@usf.edu>
2024-08-30 16:54:06 -0400

    "Task 3 - Financial Calculations"

--- cc_.js
+++ cc_.js
@@ -8,10 +8,28 @@ console.log(employeeID,typeof(employeeID));
 console.log(isActive,typeof(isActive));
 
 //Task 2: Compound Data Types
+
 let products = ["brownies","cookies","cakes"];
 const productDetails = {
 name: "brownies",
 price: 540,
-inStock:true,
+inStock: true
 }
 console.log(products,productDetails);
+
+// Task 3: Assignment Operators
+let accountBalance = 50;
+
+accountBalance += 1
+    console.log(accountBalance)
+accountBalance -= 30
+    console.log(accountBalance)
+
+accountBalance *= 3
+    console.log(accountBalance)
+
+accountBalance /= 2
+    console.log(accountBalance)
+
+accountBalance %= 2
+    console.log(accountBalance)
