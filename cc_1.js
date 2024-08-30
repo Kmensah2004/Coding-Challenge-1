f6696090ad8354af1af2b190cd1866abd8e94b7b
Kosi Mensah <kmensah@usf.edu>
2024-08-30 16:31:34 -0400

    Intial Commit - JavaScripts Basics Challenge Setup

--- cc_1.js
+++ cc_1.js
@@ -1,8 +1,1 @@
-//Task 1 : Variables and Data Types
 
-let employeeName = "Kosi Mensah";
-const employeeID = 642;
-var isActive = true;
-console.log(employeeName,typeof(employeeName));
-console.log(employeeID,typeof(employeeID));
-console.log(isActive,typeof(isActive));
