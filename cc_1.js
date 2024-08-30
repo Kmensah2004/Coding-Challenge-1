64c23472c222b7c71746b5738691ffc8f73763c3
Kosi Mensah <kmensah@usf.edu>
2024-08-30 16:29:19 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+//Task 1 : Variables and Data Types
+
+let employeeName = "Kosi Mensah";
+const employeeID = 642;
+var isActive = true;
+console.log(employeeName,typeof(employeeName));
+console.log(employeeID,typeof(employeeID));
+console.log(isActive,typeof(isActive));
